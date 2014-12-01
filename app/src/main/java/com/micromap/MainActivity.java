036c1cc8ac0061610ad84ce1b9ca18d0bbc4f3c8
pingfuf <pingfuf@gmail.com>
2014-12-02 00:04:38 +0800

    修改了代码

--- app/src/main/java/com/micromap/MainActivity.java
+++ app/src/main/java/com/micromap/MainActivity.java
@@ -1,22 +1,17 @@
 package com.micromap;
 
-import android.content.Intent;
-import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.micromap.view.BaseActivity;
 
-
-public class MainActivity extends BaseActivity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        gotoActivity(MapActivity.class);
     }
 
 
@@ -40,22 +35,5 @@ public class MainActivity extends BaseActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * 跳转到另一个界面
-     *
-     * @param activityClass 要跳转的activityClass
-     */
-    private void gotoActivity(final Class activityClass) {
-        new Handler().postDelayed(new Runnable() {
-            public void run() {
-                Intent mainIntent = new Intent(MainActivity.this, activityClass);
-                startActivity(mainIntent);
-                finish();
-                overridePendingTransition(android.R.anim.fade_in,
-                        android.R.anim.fade_out);
-            }
-        }, 1500);
     }
 }
